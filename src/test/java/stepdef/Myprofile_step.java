379c815bf6bff5e73b87ae1af759b6f3fa10732d
Md Nazmul Hossain <nazmulny15@gmail.com>
2021-10-13 23:11:30 -0400

    homework2

---
+++ src/test/java/stepdef/Myprofile_step.java
@@ -0,0 +1,9 @@
+package stepdef;
+
+import io.cucumber.java.en.Then;
+
+public class Myprofile_step {
+    @Then("I should not be able to successfully log in")
+    public void iShouldNotBeAbleToSuccessfullyLogIn() {
+    }
+}
---
+++ src/test/java/stepdef/login_Step.java
@@ -0,0 +1,26 @@
+package stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class login_Step {
+    @Given("I am at TalentTEK Homepage")
+    public void iAmAtTalentTEKHomepage() {
+        
+    }
+
+    @And("I enter a invalid email address")
+    public void iEnterAInvalidEmailAddress() {
+        
+    }
+
+    @And("I enter a valid password")
+    public void iEnterAValidPassword() {
+        
+    }
+
+    @When("I click on Login Button")
+    public void iClickOnLoginButton() {
+    }
+}
